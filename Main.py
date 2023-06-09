c99cd9ced3e9871412cbfeb8f706224f06ddead4
Douglas Arthur <douglasarthur.s.bento@gmail.com>
2023-06-09 00:32:17 -0400

    login e inicial prontos

--- Main.py
+++ Main.py
@@ -2,85 +2,10 @@ from classe import *
 from funcoes import *
 from os import system
 from login import login
+from initial import initial
 
-# DOIDEIRA DO ROBSON
-i = 0
-a = 0
-alunos_Cadastrados = []
-login.login()
-while True:
-    
-    nome = input('Nome do Aluno:\n')
-    data_N = input('Data de Nascimento do Aluno:\n')
-    n_Matricula = input('Nº da Matricula do Aluno:\n')
-    turma = input('Turma e Turno do Aluno:\n')
-    sexo = input('Sexo do Aluno:\n')
-    system('cls')
-
-    Atleta = cadastro(nome,data_N,n_Matricula,turma,sexo)
-    alunos_Cadastrados.insert(i, Atleta)
-    i =+ 1
-
-    escolha = input('Deseja adicionar outro aluno?\n[1] Sim\n[2] Não\n')
-    system('cls')
-    if escolha == '1':
-        continue
-    elif escolha == '2':
-        break
-    while escolha != '1' and escolha != '2':
-        escolha = input('Deseja adicionar outro aluno?\n[1] Sim\n[2] Não\n')
-        system('cls')
-        if escolha == '1':
-            break
-        elif escolha == '2':
-            break
-    if escolha == '2':
-        break
-
-for i in range(len(alunos_Cadastrados)):
-    alunos_Cadastrados[i].exibir()
-    while True:
-        info = input('Deseja alterar alguma informação?\n[1] Sim\n[2] Não\n')
-        system('cls')
-        while info != '1' and info != '2':
-            info = input('Deseja alterar alguma informação?\n[1] Sim\n[2] Não\n')
-            system('cls')
-            if info == '2':
-                break
-        if info == '1':
-            modaca = input('Qual informação deseja alterar\n[1] Nome\n[2] Data Nasc.\n[3] Nº Matri.\n[4] Turma\Turno\n[5] Sexo\n')
-            indice = ['1','2','3','4','5']
-            while modaca not in indice:
-                modaca = input('Qual informação deseja alterar\n[1] Nome\n[2] Data Nasc.\n[3] Nº Matri.\n[4] Turma\Turno\n[5] Sexo\n')
-            if modaca == indice[0]:
-                nome = input('Nome do Aluno:\n')
-                alunos_Cadastrados[i].setNome(nome)
-                alunos_Cadastrados[i].exibir()
-
-            elif modaca == indice[1]:
-                data_N = input('Data de Nascimento do Aluno:\n')
-                alunos_Cadastrados[i].setData_N(data_N)
-                alunos_Cadastrados[i].exibir()
-
-            elif modaca == indice[2]:
-                n_Matricula = input('Nº da Matricula do Aluno:\n')
-                alunos_Cadastrados[i].setN_Matricula(n_Matricula)
-                alunos_Cadastrados[i].exibir()
-
-            elif modaca == indice[3]:
-                turma = input('Turma e Turno do Aluno:\n')
-                alunos_Cadastrados[i].setTurma(turma)
-                alunos_Cadastrados[i].exibir()
-
-            elif modaca == indice[4]:
-                sexo = input('Sexo do Aluno:\n')
-                alunos_Cadastrados[i].setSexo(sexo)
-                alunos_Cadastrados[i].exibir()
-            
-        elif info == '2':
-            break
-        
-    system("cls")     
+def relatorio():   
+    global alunos_Cadastrados
     relatorio = input("deseja gerar um relatório:\n[1]Sim\n[2]Não\n")
     system('cls')
     while True:
@@ -107,6 +32,93 @@ for i in range(len(alunos_Cadastrados)):
                 text("Finalizando....")
                 break
         break
+
+# DOIDEIRA DO ROBSON
+i = 0
+a = 0
+alunos_Cadastrados = []
+login.login()
+
+retorno = initial()
+
+
+if retorno == "3":
+    relatorio()
+elif retorno == "1":
+    while True:
+        
+        nome = input('Nome do Aluno:\n')
+        data_N = input('Data de Nascimento do Aluno:\n')
+        n_Matricula = input('Nº da Matricula do Aluno:\n')
+        turma = input('Turma e Turno do Aluno:\n')
+        sexo = input('Sexo do Aluno:\n')
+        system('cls')
+
+        Atleta = cadastro(nome,data_N,n_Matricula,turma,sexo)
+        alunos_Cadastrados.insert(i, Atleta)
+        i =+ 1
+
+        escolha = input('Deseja adicionar outro aluno?\n[1] Sim\n[2] Não\n')
+        system('cls')
+        if escolha == '1':
+            continue
+        elif escolha == '2':
+            break
+        while escolha != '1' and escolha != '2':
+            escolha = input('Deseja adicionar outro aluno?\n[1] Sim\n[2] Não\n')
+            system('cls')
+            if escolha == '1':
+                break
+            elif escolha == '2':
+                break
+        if escolha == '2':
+            break
+
+    for i in range(len(alunos_Cadastrados)):
+        alunos_Cadastrados[i].exibir()
+        while True:
+            info = input('Deseja alterar alguma informação?\n[1] Sim\n[2] Não\n')
+            system('cls')
+            while info != '1' and info != '2':
+                info = input('Deseja alterar alguma informação?\n[1] Sim\n[2] Não\n')
+                system('cls')
+                if info == '2':
+                    break
+            if info == '1':
+                modaca = input('Qual informação deseja alterar\n[1] Nome\n[2] Data Nasc.\n[3] Nº Matri.\n[4] Turma\Turno\n[5] Sexo\n')
+                indice = ['1','2','3','4','5']
+                while modaca not in indice:
+                    modaca = input('Qual informação deseja alterar\n[1] Nome\n[2] Data Nasc.\n[3] Nº Matri.\n[4] Turma\Turno\n[5] Sexo\n')
+                if modaca == indice[0]:
+                    nome = input('Nome do Aluno:\n')
+                    alunos_Cadastrados[i].setNome(nome)
+                    alunos_Cadastrados[i].exibir()
+
+                elif modaca == indice[1]:
+                    data_N = input('Data de Nascimento do Aluno:\n')
+                    alunos_Cadastrados[i].setData_N(data_N)
+                    alunos_Cadastrados[i].exibir()
+
+                elif modaca == indice[2]:
+                    n_Matricula = input('Nº da Matricula do Aluno:\n')
+                    alunos_Cadastrados[i].setN_Matricula(n_Matricula)
+                    alunos_Cadastrados[i].exibir()
+
+                elif modaca == indice[3]:
+                    turma = input('Turma e Turno do Aluno:\n')
+                    alunos_Cadastrados[i].setTurma(turma)
+                    alunos_Cadastrados[i].exibir()
+
+                elif modaca == indice[4]:
+                    sexo = input('Sexo do Aluno:\n')
+                    alunos_Cadastrados[i].setSexo(sexo)
+                    alunos_Cadastrados[i].exibir()
+                
+            elif info == '2':
+                break
+            
+        system("cls")
+    
 
 
 #i = 0
--- login/login.py
+++ login/login.py
@@ -1,93 +1,72 @@
-from tkinter import *
-from tkinter import messagebox
 import os
-def login():
-    master = Tk()
-    master.title("Cadastro/Login")
-    master.geometry("1000x500")
-    master.resizable(width=1, height= 1)
+from time import sleep
 
-    #funções
+def login():
     def cad():
-        matricula=cad_token.get()
-        senha=cad_senha.get()
+        matricula = input("Digite o número de matricula\nR: ")
+        senha = input("Digite sua senha\nR: ")
         teste = 0
         if matricula.isdigit() == True:
             with open ("usuarios.txt","r", encoding="utf-8") as arquivo:
                 usuarios = arquivo.readlines()
             for linha in usuarios:
-                if matricula and senha in linha:
-                    messagebox.showerror("ERRO DE CADASTRO", message="ESTE USUARIO JÁ EXISTE\nTENTE OUTRO NOME E SENHA")
+                while matricula and senha in linha:
+                    print("ERRO DE CADASTRO\nESTE USUARIO JÁ EXISTE\nTENTE OUTRO NOME E SENHA")
+                    matricula = input("Digite o número de matricula\nR: ")
+                    senha = input("Digite sua senha\nR: ")
                     teste +=1
-            if matricula == senha:
-                messagebox.showerror(title="ERRO DE CADASTRO", message="O nome de usuario não pode ser igual a senha")
+            while matricula == senha:
+                print("ERRO DE CADASTRO\nO nome de usuario não pode ser igual a senha")
                 teste +=1
-            elif teste == 0:
-                messagebox.showinfo(title="CADASTRO APROVADO!", message="BEM VINDO, {}!".format(matricula))
+                matricula = input("Digite o número de matricula\nR: ")
+                senha = input("Digite sua senha\nR: ")
+            if teste == 0:
+                print(f"CADASTRO APROVADO!\nBEM VINDO, {matricula}!")
                 with open ("usuarios.txt","a", encoding="utf-8") as arquivo:
                     arquivo.write(str(matricula) + (senha)+ ("\n"))
-        else:
-            messagebox.showerror(title="ERRO DE CADASTRO", message="Matricula deve conter somente números")
+        while matricula.isdigit() == False:
+            print("ERRO DE CADASTRO\nMatricula deve conter somente números")
+            matricula = input("Digite o número de matricula\nR: ")
+            senha = input("Digite sua senha\nR: ")
 
 
 
 
     def log():
-        matricula = en_token.get()
-        senha = en_senha.get()
-        if matricula.isdigit() == True:
-            with open ("usuarios.txt","r", encoding="utf-8") as arquivo:
-                usuarios = arquivo.readlines()
-            for linha in usuarios:
-                if matricula and senha in linha:
-                    messagebox.showinfo(title="LOGIN APROVADO!", message="BOM DE VER AQUI, {}!".format(matricula))
+        cancelar = 0
+        while cancelar == 0:
+            matricula = input("Digite o número de matricula\nR: ")
+            senha = input("Digite sua senha\nR: ")
+            if matricula.isdigit() == True:
+                with open ("usuarios.txt","r", encoding="utf-8") as arquivo:
+                    usuarios = arquivo.readlines()
+                for linha in usuarios:
+                    if matricula and senha in linha:
+                        print(f"LOGIN APROVADO!\nBOM TE VER AQUI, {matricula}!")
+                        cancelar+=1
 
-                else:
-                    messagebox.showerror(title="LOGIN NEGADO!", message="USUARIO OU SENHA INVALIDO!")
-        else:
-            messagebox.showerror(title="ERRO DE CADASTRO", message="Matricula deve conter somente números")
+                    else:
+                        print("LOGIN NEGADO!\nUSUARIO OU SENHA INVALIDO!")
+                        matricula = input("Digite o número de matricula\nR: ")
+                        senha = input("Digite sua senha\nR: ")
 
+            while matricula.isdigit() == False:
+                print("ERRO DE CADASTRO\nMatricula deve conter somente números")
+                matricula = input("Digite o número de matricula\nR: ")
+                senha = input("Digite sua senha\nR: ")
 
-    #variaveis globais
-    pastapp = os.path.dirname(__file__)
-    esconda_senha = StringVar
-
-    #import img
-    img_fundo = PhotoImage(file=pastapp+"\\tela_final.png")
-    botao = PhotoImage(file=pastapp+"\\botão.png")
-    sign = PhotoImage(file=pastapp+"\\sign_up.png")
-
-
-    #Lables
-    lab_fundo = Label(master, image=img_fundo)
-    lab_fundo.place (x=0, y=0)
-
-
-    #caixas de entrada
-    en_token = Entry(master, bd=2, font=("Calibri", 15), justify=CENTER)
-    en_token.place(width=439, height=44, x=32, y=134)
-
-    cad_token = Entry(master, bd=2, font=("Calibri", 15), justify=CENTER)
-    cad_token.place(width=437, height=43, x=532, y=135)
-
-    en_senha = Entry(master, textvariable=esconda_senha, show="*", bd=2, font=("Calibri",15), justify=CENTER)
-    en_senha.place(width=436, height=41, x=34, y=231)
-
-    cad_senha = Entry(master, textvariable=esconda_senha, show="*", bd=2, font=("Calibri",15), justify=CENTER)
-    cad_senha.place(width=436, height=44, x=534, y=230)
-
-    #botões
-
-    bt_entrar = Button(master, bd=0, image=botao, command=log)
-    bt_entrar.place(width=140, height=29, x=181, y=397)
-
-    bt_entrar1 = Button(master, bd=0, image=botao, command=cad)
-    bt_entrar1.place(width=141, height=32, x=682, y=396)
-
-    #função2
-
-
-    master.mainloop()
-
+    print("━━━━━━━━━━━━━━━━❪❂❫━━━━━━━━━━━━━━━━")
+    print("             BEM VINDO             ")
+    print("━━━━━━━━━━━━━━━━❪❂❫━━━━━━━━━━━━━━━━")
+    sleep(1)
+    print("Deseja fazer login?\nOu cadastro?")
+    escolha = input("[1]Login\n[2]Cadastro\nR:")
+    while escolha.isnumeric() == False or int(escolha) > 2:
+        print("Digite um número válido\nTente novamente")
+        escolha = input("[1]Login\n[2]Cadastro\nR:")
+    if escolha == "1":
+        log()
+    else:
+        cad()
 
 
