dd2f25e1f72e28f4a2c8dbe5d636522436d8f5e1
WandellAZ <wandelldacostamota@gmail.com>
2023-06-11 17:49:17 -0400

    Finalizado(Igor)

--- Classe.py
+++ Classe.py
@@ -1,27 +1,76 @@
 from funcoes import *
+from login import DataBaser
+import DataBaserAlunos
 
-class cadastro():
-    def __init__(self, nome, data_N, n_Matricula, turma, sexo):
+
+class Alunos():
+    def __init__(self, nome, data_N, n_Matricula, turma, sexo, modalidade):
         self.nome = nome
         self.data_N = data_N
         self.n_Matricula = n_Matricula
         self.turma = turma
         self.sexo = sexo
+        self.modalidade = modalidade
 
-    def exibir(self):
-        text(f'Nome: {self.nome}\nData de Nascimento: {self.data_N}\nNº Matricula: {self.n_Matricula}\nTurma: {self.turma}\nSexo: {self.sexo}')
+    def cad(self):
+        DataBaserAlunos.conn.execute("""
+            INSERT INTO Alunos(matricula, nome, data_nascimento, turma, modalidade, sexo) VALUES(?,?,?,?,?,?)
+        """,(self.n_Matricula, self.nome, self.data_N, self.turma, self.modalidade, self.sexo))
+        DataBaserAlunos.conn.commit()
+        print("Cadastrado com sucesso.")
+    
+    def relatorio(self):
+        cursor = DataBaserAlunos.conn.execute("""
+        SELECT * FROM Alunos
+        WHERE matricula = ?
+        """,(self.n_Matricula,))
+        alunos = cursor.fetchall()
+        if len(alunos) > 0:
+            print("Informações do aluno:")
+            for aluno in alunos:
+                print(f"""
+                matricula: {aluno[1]}
+                nome: {aluno[2]}
+                data_nascimento: {aluno[3]}
+                turma: {aluno[4]}
+                modalidade: {aluno[5]}
+                sexo: {aluno[6]}
+                """)
+        else:
+            print("Nenhum aluno encontrado com a matrícula fornecida.")
 
-    def setNome(self, nome):
-        self.nome = nome
 
-    def setData_N(self, data_N):
-        self.data_N = data_N
+    
+class User():
+    def __init__(self, matricula, senha, email):
+        self.matricula = matricula
+        self.senha = senha
+        self.email = email
 
-    def setN_Matricula(self, n_Matricula):
-        self.n_Matricula = n_Matricula
+    def registerToDataBase(self):
 
-    def setTurma(self, turma):
-        self.turma = turma
+        DataBaser.cursor.execute("""
+        INSERT INTO Users(matricula, password, email) VALUES(?, ?, ?)
+        """, (self.matricula, self.senha, self.email))
+        DataBaser.conn.commit()
+        print("Registrado com sucesso")
+    
+    def log(self):
+        DataBaser.cursor.execute("""
+        SELECT * FROM Users
+        WHERE (matricula = ? AND password = ?)
+        """, (self.matricula, self.senha))
+        verify = DataBaser.cursor.fetchone()
+        try:
+            if (self.matricula in verify and self.senha in verify):
+                print("Login realizado, Bem vindo")
+                return True
 
-    def setSexo(self, sexo):
-        self.sexo = sexo
+        except:
+            print("Login negado, matricula ou senha estão errados")
+            quit()
+            
+            
+
+        
+
--- funcoes.py
+++ funcoes.py
@@ -10,3 +10,50 @@ def text(txt):
     sleep(0.05)
     print(end = '\n')
     system('cls')
+    def escolher_modalidade():
+        modalidade = [
+        "ARREMESSO DE PESO",
+        "LANÇAMENTO DE DARDO",
+        "SALTO EM DISTÂNCIA",
+        "LANÇAMENTO DE DISCO",
+        "SALTO EM ALTURA",
+        "SALTO TRIPLO",
+        "100 METROS",
+        "200 METROS",
+        "400 METROS",
+        "800 METROS",
+        "1500 METROS",
+        "3000 METROS (só moças)",
+        "5000 METROS (só rapazes)",
+        "4 X 100 METROS",
+        "4 X 400 METROS",
+        ]
+
+        print("Escolha a modalidade ")
+
+        print(""" 
+        [1] ARREMESSO DE PESO
+        [2] LANÇAMENTO DE DARDO
+        [3] SALTO EM DISTÂNCIA
+        [4] LANÇAMENTO DE DISCO
+        [5] SALTO EM ALTURA
+        [6] SALTO TRIPLO
+        [7] 100 METROS
+        [8] 200 METROS
+        [9] 400 METROS
+        [10] 800 METROS
+        [11] 1500 METROS
+        [12] 3000 METROS (só moças)
+        [13] 5000 METROS (só rapazes)
+        [14] 4 X 100 METROS 
+        [15] 4 X 400 METROS 
+        """)
+
+        escolha_da_modalidade = input("R: ")
+
+        while escolha_da_modalidade.isnumeric() == False or int(escolha_da_modalidade) > 15 or int(escolha_da_modalidade) < 1:
+            print("Número invalido\nTente novamnte")
+            escolha_da_modalidade = input("R: ")
+        escolha = int(escolha_da_modalidade) - 1
+
+        return modalidade [escolha]
