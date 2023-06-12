e3711fe8e6fc50bd0ee9a90554f4a7a5ba08d15c
JeffUwU <jeffersonbruno0210@gmail.com>
2023-06-11 21:44:12 -0300

    Finalizado

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ from funcoes import *
 from os import system
 from login import login
 from login import *
+from datetime import datetime
+
 
 i = 0
 a = 0
@@ -18,18 +20,35 @@ if retorno == "3":
     matricula = input("Matricula:")
     aluno = Alunos(None,None,matricula,None,None,None)
     aluno.relatorio()
+   
+        
 
 elif retorno == "1":
-    print("Cadastre o aluno aqui")
-    matricula = input("Matricula: ")
-    nome = input("Nome: ")
-    data = input("Data: ")
-    turma = escolher_turma()
-    sexo = input("Sexo: ")
-    modalidade = input("Modalidade: ")
+    def data():
+        while True:
+            data_str = input("Digite sua data de nascimento (DD/MM/AAAA): ")
+            try:
+                data = datetime.strptime(data_str, "%d/%m/%Y")
+                return data
+            except ValueError:
+                print("Formato inválido. Digite a data no formato DD/MM/AAAA.")
 
-    aluno = Alunos(nome, data, matricula, turma, sexo, modalidade)
-    aluno.cad()
+    
+    while True:
+        print("Cadastre o aluno aqui")
+        matricula = input("Matricula: ")
+        nome = input("Nome: ")
+        data_nascimento= data()
+        turma = escolher_turma()
+        sexo = input("Sexo: ")
+        modalidade = input("Modalidade: ")
+
+        aluno = Alunos(nome, data_nascimento.strftime("%d/%m/%Y"), matricula, turma, sexo, modalidade)
+        aluno.cad()
+        reiniciar = input("Deseja fazer mais algum relatorio?\n (S/N): ")
+        if reiniciar.lower() !="s":
+            break
+
 
 elif retorno == "4":
     system('cls')
